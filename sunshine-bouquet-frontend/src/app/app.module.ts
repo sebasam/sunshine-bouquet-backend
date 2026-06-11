0b71c01333699306908ff7c4aa016d8b94afb46d
Sebas Amaya <sebasamayaxd@hotmail.com>
2026-06-11 09:19:45 -0500

    feat(frontend): init angular module architecture with bootstrap and apollo
    Generated Angular workspace with scss and routing. Configured Apollo Client targeting the gateway port with explicit providers to ensure DI stability.

---
+++ sunshine-bouquet-frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    GraphQLModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
